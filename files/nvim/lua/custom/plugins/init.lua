3c788cfc7c6205551dcabca7e1a1a57b4cfc5ee2
Rienovie <fisher.vince@gmail.com>
2024-12-26 21:11:46 -0500

    12.26.24

--- files/nvim/lua/custom/plugins/init.lua
+++ files/nvim/lua/custom/plugins/init.lua
@@ -18,7 +18,19 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = { signs = false },
 	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
 
+			-- Only one of these is needed.
+			"nvim-telescope/telescope.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+			"echasnovski/mini.pick", -- optional
+		},
+		config = true,
+	},
 	-- Local version to use when editing
 	-- { "Basher", dir = "~/projects/Basher", opts = { funOnStart = false, pathMaxDirs = 1 } },
 	-- Version to test from git
