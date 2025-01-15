f9866304c4204f7c787f6acb2491cf4e22b69db0
Rienovie <fisher.vince@gmail.com>
2025-01-14 20:13:12 -0500

    1.14.25

--- scripts/autostart_gnome.sh
+++ scripts/autostart_gnome.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-if [ "$XDG_SESSION_DESKTOP" = "GNOME" ]; then
+if [ "$XDG_SESSION_DESKTOP" = "gnome" ]; then
     steam -silent >/dev/null 2>&1 &
     discord --start-minimized >/dev/null 2>&1
 fi
