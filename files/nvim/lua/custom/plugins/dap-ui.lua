b13523218ca081b4004721055f81f111547b6698
Rienovie <fisher.vince@gmail.com>
2025-01-22 16:46:47 -0500

    1.22.25

--- files/nvim/lua/custom/plugins/dap-ui.lua
+++ files/nvim/lua/custom/plugins/dap-ui.lua
@@ -11,48 +11,58 @@ return {
 			require("dapui").setup()
 
 			local dap = require("dap")
-			dap.adapters.cppdbg = {
-				id = "cppdbg",
+			dap.adapters.gdb = {
 				type = "executable",
-				command = "/home/vince/Apps/cppDebug/extension/debugAdapters/bin/OpenDebugAD7",
+				command = "gdb",
+				args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
 			}
 			dap.configurations.cpp = {
+				-- {
+				-- 	name = "Launch file",
+				-- 	type = "gdb",
+				-- 	request = "launch",
+				-- 	program = function()
+				-- 		return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				-- 	end,
+				-- 	cwd = "${workspaceFolder}",
+				-- 	stopAtEntry = true,
+				-- 	setupCommands = {
+				-- 		{
+				-- 			text = "-enable-pretty-printing",
+				-- 			description = "enable pretty printing",
+				-- 			ignoreFailures = false,
+				-- 		},
+				-- 	},
+				-- },
 				{
-					name = "Launch file",
-					type = "cppdbg",
+					name = "Launch",
+					type = "gdb",
 					request = "launch",
 					program = function()
 						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
 					end,
 					cwd = "${workspaceFolder}",
-					stopAtEntry = true,
-					setupCommands = {
-						{
-							text = "-enable-pretty-printing",
-							description = "enable pretty printing",
-							ignoreFailures = false,
-						},
-					},
+					stopAtBeginningOfMainSubprogram = false,
 				},
-				{
-					name = "Attach to gdbserver :1234",
-					type = "cppdbg",
-					request = "launch",
-					MIMode = "gdb",
-					miDebuggerServerAddress = "localhost:1234",
-					miDebuggerPath = "/usr/bin/gdb",
-					cwd = "${workspaceFolder}",
-					program = function()
-						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-					end,
-					setupCommands = {
-						{
-							text = "-enable-pretty-printing",
-							description = "enable pretty printing",
-							ignoreFailures = false,
-						},
-					},
-				},
+				-- {
+				-- 	name = "Attach to gdbserver :1234",
+				-- 	type = "cppdbg",
+				-- 	request = "launch",
+				-- 	MIMode = "gdb",
+				-- 	miDebuggerServerAddress = "localhost:1234",
+				-- 	miDebuggerPath = "/usr/bin/gdb",
+				-- 	cwd = "${workspaceFolder}",
+				-- 	program = function()
+				-- 		return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				-- 	end,
+				-- 	setupCommands = {
+				-- 		{
+				-- 			text = "-enable-pretty-printing",
+				-- 			description = "enable pretty printing",
+				-- 			ignoreFailures = false,
+				-- 		},
+				-- 	},
+				-- },
 			}
 		end,
 	},
--- scripts/kitty_launch.sh
+++ scripts/kitty_launch.sh
@@ -2,6 +2,7 @@
 
 randomBackground=$(find "/home/vince/.config/kitty/backgrounds" -type f | shuf -n 1)
 
-randomColor=$(shuf -n 1 "/home/vince/.config/kitty/colors.txt")
+# bgColor=$(shuf -n 1 "/home/vince/.config/kitty/colors.txt")
+bgColor="#052a06"
 
-kitty -o background="$randomColor" -o background_image="$randomBackground" "$@"
+kitty -o background="$bgColor" -o background_image="$randomBackground" "$@"
