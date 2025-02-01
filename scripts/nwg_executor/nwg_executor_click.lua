90efee71483b72be6fba89e6f3876b7ce54eeeaa
Rienovie <fisher.vince@gmail.com>
2025-01-31 23:41:09 -0500

    1.31.25

--- scripts/nwg_executor/nwg_executor_click.lua
+++ scripts/nwg_executor/nwg_executor_click.lua
@@ -1,10 +1,12 @@
 for line in io.lines("/home/vince/Scripts/nwg_executor/nwg_executor_value") do
 	if line == "Updates are available!" then
-		os.execute('kitty sh -c "eos-update && read -p "Updates have finished..."')
+		local subS = "'Updates have finished...'"
+		os.execute('kitty sh -c "eos-update && read -p ' .. subS .. '"')
 		os.execute("lua /home/vince/Scripts/nwg_executor/nwg_executor_update.lua")
 		return
 	elseif line == "Up To Date" then
 		os.execute("lua /home/vince/Scripts/nwg_executor/nwg_executor_update.lua")
 		return
 	end
+	print(line)
 end
