bcc12d18e149bac1cb77809d45d92a5dbfb03f2a
Rienovie <fisher.vince@gmail.com>
2025-02-22 20:57:07 -0500

    2.22.25

--- scripts/kitty_launch.sh
+++ scripts/kitty_launch.sh
@@ -2,7 +2,8 @@
 
 randomBackground=$(find "/home/vince/.config/kitty/backgrounds" -type f | shuf -n 1)
 
-bgColor=$(shuf -n 1 "/home/vince/.config/kitty/colors.txt")
+# bgColor=$(shuf -n 1 "/home/vince/.config/kitty/colors.txt")
 # bgColor="#052a06" # green
+bgColor="#00394b" # blue-ish
 
 kitty -o background="$bgColor" -o background_image="$randomBackground" "$@"
