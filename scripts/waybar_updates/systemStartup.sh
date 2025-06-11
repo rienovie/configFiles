0417496dcabed35eb433ba0915b46272cfcf2887
Rienovie <fisher.vince@gmail.com>
2025-06-11 18:34:10 -0400

    6.11.25

--- scripts/waybar_updates/systemStartup.sh
+++ scripts/waybar_updates/systemStartup.sh
@@ -8,18 +8,18 @@ fi
 echo $runOnce
 
 iconUpToDate=$"󰕥"
-iconChecking=$"󱆢"
-iconUpdate=$"󰻌"
+iconChecking=$"󱆢 Checking for updates..."
+iconUpdate=$"󰻌 Updates are available!"
 
 while :; do
-    echo "$iconChecking Checking for updates..." > ~/Scripts/waybar_updates/updateValue
+    echo "$iconChecking" > ~/Scripts/waybar_updates/updateValue
     # sleep 2 so it shows in the panel that it's checking
     sleep 2
     if checkupdates; then
-        echo "$iconUpdate Updates are available!" > ~/Scripts/waybar_updates/updateValue
+        echo "$iconUpdate" > ~/Scripts/waybar_updates/updateValue
         play /home/vince/Music/sounds/boot.ogg
     else
-        echo "$iconUpToDate Up to date." > ~/Scripts/waybar_updates/updateValue
+        echo "$iconUpToDate" > ~/Scripts/waybar_updates/updateValue
     fi
 
     if $runOnce; then
