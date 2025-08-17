ccacbecb3570ef3b69181e510c0c12c0b6ab92b2
Rienovie <fisher.vince@gmail.com>
2025-08-16 21:00:37 -0400

    8.16.25

---
+++ scripts/fish/laptop_rotate.fish
@@ -0,0 +1,16 @@
+#!/usr/bin/env fish
+
+killall waybar
+
+set config_file "/home/vince/.config/niri/config.kdl"
+set current_transform (grep "transform" $config_file | sed 's/.*transform "\(.*\)".*/\1/')
+
+if test "$current_transform" = "normal"
+    set new_transform "90"
+    waybar -c ~/.config/waybar/laptop_vertical_config.jsonc &
+else
+    set new_transform "normal"
+    waybar &
+end
+
+sed -i "s/transform \".*\"/transform \"$new_transform\"/" $config_file
