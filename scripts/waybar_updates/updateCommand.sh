a50f61f67f6e0c1149b0349f008b20eff001366f
rienovie <fisher.vince@gmail.com>
2025-11-12 06:20:07 -0500

    11.12.25 Desktop

--- scripts/waybar_updates/updateCommand.sh
+++ scripts/waybar_updates/updateCommand.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
 
-# paru calls AUR but don't want to use it for now
-sudo pacman -Syu && flatpak update && read -p 'Updates have finished...'
+sudo paru && flatpak update && read -p 'Updates have finished...'
