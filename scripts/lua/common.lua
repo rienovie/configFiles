863cb8a6b1563a6c61c0277adc7e345886e83bb0
rienovie <fisher.vince@gmail.com>
2025-11-14 20:31:38 -0500

    11.14.25

---
+++ scripts/lua/common.lua
@@ -0,0 +1,37 @@
+local export = {}
+
+function export.run(cmd)
+	local h = io.popen(cmd .. " 2>&1", "r")
+	if not h then
+		return {
+			ok = false,
+			exit = 1,
+			output = "Could not open pipe",
+		}
+	end
+	local out = h:read("*a")
+	local ok, _, code = h:close()
+	return {
+		ok = ok,
+		exit = code,
+		output = out,
+	}
+end
+
+function export.scanDir(dir, hidden)
+	local files = {}
+	local cmd = "ls -1 " .. (hidden and "-a " or "") .. dir
+	for file in io.popen(cmd):lines() do
+		table.insert(files, file)
+	end
+	return files
+end
+
+function export.printTable(t)
+	print("Table:")
+	for k, v in pairs(t) do
+		print("  " .. k .. ": " .. v)
+	end
+end
+
+return export
---
+++ scripts/lua/test.lua
@@ -0,0 +1,7 @@
+-- needed for running from anywhere
+-- TODO: look into adding to default path
+package.path = package.path .. ";/home/vince/Scripts/lua/?.lua"
+
+local common = require("common")
+
+common.printTable(common.scanDir("/home/vince/Scripts/lua"))
